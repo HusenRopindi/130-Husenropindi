d91d0edd577e5c5c5b3a3e36f21ae4d1759b01dd
HusenRopindi <144459720+HusenRopindi@users.noreply.github.com>
2023-09-08 20:53:46 +0700

    Add files via upload

---
+++ import math.py
@@ -0,0 +1,10 @@
+import math
+
+# Input jari-jari bola
+jari_jari = float(input("Masukkan jari-jari bola: "))
+
+# Hitung luas permukaan bola
+luas_permukaan = 4 * math.pi * jari_jari**2
+
+# Tampilkan hasil
+print("Luas permukaan bola dengan jari-jari", jari_jari, "adalah:", luas_permukaan)
